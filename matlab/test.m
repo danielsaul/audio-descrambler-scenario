61380020193e4ae60efb087022898b71fa934601
Daniel Saul <daniel@dansaul.co.uk>
2015-12-14 15:31:25 +0000

    sine multiplication

--- matlab/test.m
+++ matlab/test.m
@@ -3,57 +3,61 @@ disp(fs)
 N = length(ori_y);
 
 % Original time domain
-figure(1);
+%figure(1);
 t = 1/fs:1/fs:N/fs;
-plot(t,ori_y); 
+%plot(t,ori_y); 
 
 % Original freq domain
-figure(2);
+%figure(2);
 f = linspace(-fs/2,fs/2,N);
 ori_Y = fft(ori_y,N);
-plot(f,fftshift(abs(ori_Y)));
+%plot(f,fftshift(abs(ori_Y)));
 
 [scr_y,fs] = audioread('scrambled.wav');
 N = length(scr_y);
 
 % Scrambled time domain
-figure(3);
+%figure(3);
 t = 1/fs:1/fs:N/fs;
-plot(t,scr_y);
+%plot(t,scr_y);
 
 % Scrambled freq domain
-figure(4);
+%figure(4);
 f = linspace(-fs/2,fs/2,N);
 scr_Y = fft(scr_y,N);
-plot(f,fftshift(abs(scr_Y))); 
+%plot(f,fftshift(abs(scr_Y))); 
 [pk,MaxFreq] = findpeaks(fftshift(abs(scr_Y)),'NPeaks',1,'SortStr','descend');
 hold on
-plot(f(MaxFreq),pk,'or')
+%plot(f(MaxFreq),pk,'or')
 hold off
 Freq = f(MaxFreq)
+
+% Scrambled * 7kHz
+sine = sin(2*pi*7000*t);
+scr_y_sin = scr_y * sine;
+% 
+% % Scrambled * 7kHz freq domain
+figure(5);
+f = linspace(-fs/2,fs/2,N);
+scr_Y_sin = fft(scr_y_sin,N);
+plot(f,fftshift(abs(scr_Y_sin))); 
+
+
 
 h  = fdesign.lowpass('Fp,Fst,Ap,Ast', 7000, 7500, 1, 60, fs);
 Hd = design(h, 'butter');
 
 scr_y_lp = filter(Hd, scr_y);
 
-% Scrambled time domain with low pass
-figure(5);
-t = 1/fs:1/fs:N/fs;
-plot(t,scr_y_lp);
+% % Scrambled time domain with low pass
+% figure(5);
+% t = 1/fs:1/fs:N/fs;
+% plot(t,scr_y_lp);
+% 
+% % Scrambled freq domain with low pass
+% figure(6);
+% f = linspace(-fs/2,fs/2,N);
+% scr_Y_lp = fftshift(fft(scr_y_lp,N));
+% plot(f,abs(scr_Y_lp)); 
 
-% Scrambled freq domain with low pass
-figure(6);
-f = linspace(-fs/2,fs/2,N);
-scr_Y_lp = fftshift(fft(scr_y_lp,N));
-plot(f,abs(scr_Y_lp)); 
-
-scr_Y_lp_flip = [scr_Y_lp((N/2)+1:N), fliplr(scr_Y_lp((N/2)+1:N))];
-
-M = length(scr_Y_lp((N/2)+1:N))
-O = length(scr_Y_lp_flip)
-
-figure(8);
-f = linspace(-fs/2,fs/2,N);
-plot(f,abs(scr_Y_lp_flip));
 
