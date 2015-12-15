4f982dfbce769e7a92dc779f652dfc629fc256dc
Daniel Saul <daniel@dansaul.co.uk>
2015-12-15 16:39:48 +0000

    Blank filter module

---
+++ digital/firmware/filter.c
@@ -0,0 +1,8 @@
+/*
+ * Real-time Audio Descrambler Scenario
+ * Digital MSP432
+ * Daniel Saul, 2015
+ *
+ * filter.c
+ *
+ */
---
+++ digital/firmware/filter.h
@@ -0,0 +1,18 @@
+/*
+ * Real-time Audio Descrambler Scenario
+ * Digital MSP432
+ * Daniel Saul, 2015
+ *
+ * filter.h
+ *
+ */
+
+#ifndef FILTER_H
+#define FILTER_H
+
+#include "driverlib.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+
+#endif
