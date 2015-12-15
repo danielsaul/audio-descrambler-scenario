03bf858034f31c40a71e2e18f62435a7192b648c
Daniel Saul <daniel@dansaul.co.uk>
2015-12-15 17:44:22 +0000

    added filter coefficients from matlab

--- digital/firmware/filter.h
+++ digital/firmware/filter.h
@@ -15,4 +15,13 @@
 #include <stdbool.h>
 #include <math.h>
 
+const float bandstop_num[3] = {1, -1.0719921366, 1};
+const float bandstop_den[3] = {1, -1.0584530227, 0.97474027397};
+
+const float lowpass_a_num[3] = {1, 2, 1};
+const float lowpass_a_den[3] = {1, -1.499554496, 0.84821868172};
+const float lowpass_b_num[3] = {1, 2, 1};
+const float lowpass_b_den[3] = {1, -1.554785180, 0.64929543814};
+
+
 #endif
