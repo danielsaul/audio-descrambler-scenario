4144ef58d748461a7011906105f98e506faa105b
Daniel Saul <daniel@dansaul.co.uk>
2015-12-16 14:09:30 +0000

    IIR Filter Written

--- digital/firmware/filter.c
+++ digital/firmware/filter.c
@@ -6,3 +6,51 @@
  * filter.c
  *
  */
+
+#include "filter.h"
+
+//////
+// Implements Infinite Impulse Response Filter
+//////
+
+// Bandstop filter, 2nd order (1 section)
+static double bandstop(double input){
+
+  update_buffer(bandstop_buf);
+  double output = filter(input, bandstop_buf, bandstop_num, bandstop_den);
+
+  return output;
+
+}
+
+// Lowpass filter, 4th order (2 sections)
+static double lowpass(double input){
+
+  update_buffer(lowpass_a_buf);
+  double outa = filter(input, lowpass_a_buf, lowpass_a_num, lowpass_a_den);
+  update_buffer(lowpass_b_buf);
+  double output = filter(outa, lowpass_b_buf, lowpass_b_num, lowpass_b_den);
+
+  return output;
+
+}
+
+// Second-order IIR filter, direct form II
+static double filter(double x, double *w, double *b, double *a){
+
+  double w[2] = x - (a[1] * w[1]) - (a[2] * w[0]);
+  double y = (b[0] * w[2]) + (b[1] * w[1]) + (b[2] * w[0]);
+
+  return y;
+
+}
+
+// Move buffer around
+// n -> n-1
+// n-1 -> n-2
+void update_buffer(double *w){
+
+  w[0] = w[1];
+  w[1] = w[2];
+
+}
--- digital/firmware/filter.h
+++ digital/firmware/filter.h
@@ -15,13 +15,25 @@
 #include <stdbool.h>
 #include <math.h>
 
-const float bandstop_num[3] = {1, -1.0719921366, 1};
-const float bandstop_den[3] = {1, -1.0584530227, 0.97474027397};
+// 8kHz Bandstop Chebyshev Filter Coefficients
+const double bandstop_num[3] = {1, -1.0719921366, 1};
+const double bandstop_den[3] = {1, -1.0584530227, 0.97474027397};
+const double bandstop_buf[3] = {0, 0, 0};
 
-const float lowpass_a_num[3] = {1, 2, 1};
-const float lowpass_a_den[3] = {1, -1.499554496, 0.84821868172};
-const float lowpass_b_num[3] = {1, 2, 1};
-const float lowpass_b_den[3] = {1, -1.554785180, 0.64929543814};
 
+// 5kHz Low-pass Chebyshev Filter Coefficients
+const double lowpass_a_num[3] = {1, 2, 1};
+const double lowpass_a_den[3] = {1, -1.499554496, 0.84821868172};
+const double lowpass_a_buf[3] = {0, 0, 0};
+
+const double lowpass_b_num[3] = {1, 2, 1};
+const double lowpass_b_den[3] = {1, -1.554785180, 0.64929543814};
+const double lowpass_b_buf[3] = {0, 0, 0};
+
+
+static double bandstop(double input);
+static double lowpass(double input);
+static double filter(double x, double *w, double *b, double *a);
+void update_buffer(double *w);
 
 #endif
